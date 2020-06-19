19dc0ee012de5b64a22fc4bf29d1e506a735ba1e
edemarest <Lunacat7!>
2020-06-18 22:11:15 -0700

    6/18/20

--- src/_03_Printing_Binary/BinaryPrinter.java
+++ src/_03_Printing_Binary/BinaryPrinter.java
@@ -6,17 +6,55 @@ public class BinaryPrinter {
 	//Do not use the Integer.toBinaryString method!
 	//Don't be afraid to use the methods that are already complete to finish the others.
 	//Create a main method to test your methods.
+	public static void main(String[] args) {
+		byte b = 100;
+		BinaryPrinter bp = new BinaryPrinter();
+		short s = 23423;
+		bp.printByteBinary(b);
+		bp.printShortBinary(s);
+		int num = 300;
+		bp.printIntBinary(num);
+	}
 	
 	public void printByteBinary(byte b) {
-	
+	  for(int i = 7; i >= 0; i--) {
+		  System.out.print((b>>i) & 1);
+	  }
 	}
 	
 	public void printShortBinary(short s) {
-		
+		byte a= (byte)(s >> 8);
+		byte b= (byte)(s & 0b11111111);
+		System.out.println("");
+		  for(int i = 7; i >= 0; i--) {
+			  System.out.print((a>>i) & 1);
+		  }
+		  for(int i = 7; i >= 0; i--) {
+			  System.out.print((b>>i) & 1);
+		  }
 	}
 	
-	public void printIntBinary(int i) {
+	public void printIntBinary(int decimalNum) {
 		
+		String binaryStr = "";
+	    
+        do {
+            // 1. Logical right shift by 1
+            int quotient = decimalNum >>> 1;
+        
+            // 2. Check remainder and add '1' or '0'
+            if( decimalNum % 2 != 0 ){
+                binaryStr = '1' + binaryStr;
+            } else {
+                binaryStr = '0' + binaryStr;
+            }
+            
+            decimalNum = quotient;
+            
+        // 3. Repeat until number is 0
+        } while( decimalNum != 0 );
+        System.out.println();
+        System.out.print(binaryStr);
 	}
 	
 	public void printLongBinary(long l) {
